49786b5fa05cf3573195c15e56cdaef3043ee3f9
Koloni-Site <yunitia210514@gmail.com>
2025-07-23 07:54:42 +0700

    Update undang_member.py

--- undang_member.py
+++ undang_member.py
@@ -84,7 +84,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
             InlineKeyboardButton("📸 Foto Hot 🔥", callback_data="foto_hot"),
             InlineKeyboardButton("🎞️ Video Hot 🎬", callback_data="video_hot")
         ],
-        [InlineKeyboardButton("🔥 Grup LiVe Omek", url="https://t.me/Livevideohot_Bot")]]
+        [
+            InlineKeyboardButton("🔥 Grup LiVe Omek", url="https://t.me/Livevideohot_Bot")
+        ]
+    ]
 
     welcome_text = (
         "🔥 *Welcome ke Zona Nakal Koloni4D!* 🔥\n\n"
